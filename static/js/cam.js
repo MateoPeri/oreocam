b5f5ca96f44dd26d9be917bf6a4a3af2f06e2967
MateoPeri <mateo@meligranamail.com>
2020-02-21 20:55:59 +0100

    Added google login

--- static/js/cam.js
+++ static/js/cam.js
@@ -64,6 +64,14 @@ function validateUser() {
     }
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
 function send_message() {
     var msg = $('#chat_input').val()
     var now = new Date().getTime();
